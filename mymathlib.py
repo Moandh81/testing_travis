5a366cdd5b0ad115347bcc9850c6e2bf39a6eb64
mohand81 <anis.dhouieb@gmail.com>
2019-05-16 08:46:36 +0200

    first commit

---
+++ mymathlib.py
@@ -0,0 +1,15 @@
+
+
+def addition(x,y):
+	return x+y
+
+
+def multiplication(x,y):
+	return x * y
+
+
+
+def soustraction(x,y):
+	return x -y
+
+
---
+++ tests.py
@@ -0,0 +1,17 @@
+import unittest
+from mymathlib import addition
+
+
+class Mytest(unittest.TestCase):
+
+
+	"""test de la fonction addition"""
+
+
+	def test_addition(self):
+		self.assertEqual(4, addition(2,2))
+
+
+
+if __name__ == "__main__":
+	unittest.main()
