0d88097656f6c429da742c1e1e37dc8839f77f31
mohand81 <anis.dhouieb@gmail.com>
2019-05-24 16:54:22 +0200

    add another testé

--- tests.py
+++ tests.py
@@ -12,6 +12,10 @@ class Mytest(unittest.TestCase):
 		self.assertEqual(4, addition(2,2))
 
 
+	def test_multiplication(self):
+		self.assertEqual(0, addition(0,2))
+
+
 
 if __name__ == "__main__":
 	unittest.main()
